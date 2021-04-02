31b89bee7503ab73f6c189f919395b47a16bdf74
Yidah <yidahit@hotmail.com>
2021-04-02 11:22:40 +0100

    Converted App to hooks Deleted manually way to asynchronously route and replace with React.Lazy

--- src/App.js
+++ src/App.js
@@ -1,72 +1,70 @@
-import React, { Component } from 'react';
-import { Route, Switch, Redirect} from 'react-router-dom';
-import {connect} from 'react-redux';
+import React, { useEffect, Suspense } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Logout from './containers/Auth/Logout/Logout';
-import * as actions from './store/actions/index'; 
-import asyncComponent from './hoc/asyncComponent/asyncComponent';
+import * as actions from './store/actions/index';
 
-// lazy loading components
-const asyncCheckout = asyncComponent(()=>{
+// lazy loading components with React.lazy introduced in React 16.6 
+// in the app with class components we built this functionality manually
+// we build the asyncComponent in folder hoc
+// in order to use lazy we need to import Suspense and wrap the routes with it 
+// and use render to indicate which component should render async
+const Checkout = React.lazy(() => {
   return import('./containers/Checkout/Checkout');
 });
 
-const asyncOrders = asyncComponent(()=>{
+const Orders = React.lazy(() => {
   return import('./containers/Orders/Orders');
 });
-const asyncAuth = asyncComponent(()=>{
+const Auth = React.lazy(() => {
   return import('./containers/Auth/Auth');
 });
 
-class App extends Component {
-  componentDidMount(){
-    this.props.onTryAutoSignup()
-  }
-  render() {
-    let routes=(
-      <Switch>
-          <Route path="/auth" component={asyncAuth} />
-          <Route path="/" exact component={BurgerBuilder} />
-          {/* For anything unknown go to root */}
-          <Redirect to="/"/>
-      </Switch>
+const app = (props) => {
+  useEffect(() => {
+    props.onTryAutoSignup();
+  }, []);
 
-    )
-    if (this.props.isAuthenticated){
-      routes =(
-        <Switch>
-          <Route path="/checkout" component={asyncCheckout} />
-          <Route path="/orders" component={asyncOrders} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={asyncAuth} />
-          <Route path="/" exact component={BurgerBuilder} />
-          {/* For anything unknown go to root */}
-            <Redirect to="/"/>
-        </Switch>
-      )
-    }
-    return (
-      <div>
-        <Layout>
-          {routes}
-        </Layout>
-      </div>
+  let routes = (
+    <Switch>
+      <Route path="/auth" render={()=><Auth/>} />
+      <Route path="/" exact component={BurgerBuilder} />
+      {/* For anything unknown go to root */}
+      <Redirect to="/" />
+    </Switch>
+  );
+  if (props.isAuthenticated) {
+    routes = (
+      <Switch>
+        <Route path="/checkout" render={()=><Checkout/>} />
+        <Route path="/orders" render={()=><Orders/>} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/auth" render={()=><Auth/>} />
+        <Route path="/" exact component={BurgerBuilder} />
+        {/* For anything unknown go to root */}
+        <Redirect to="/" />
+      </Switch>
     );
   }
-}
+  return (
+    <div>
+      <Layout><Suspense fallback={<p>Loading...</p>}>{routes}</Suspense></Layout>
+    </div>
+  );
+};
 
-const mapStateToProps = state => {
-  return{
-    isAuthenticated: state.auth.token !== null
-  }
-}
-const mapDispatchToProps = dispatch =>{
-  return{
-    onTryAutoSignup: ()=> dispatch(actions.authCheckState())
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState()),
+  };
+};
 
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(app);
