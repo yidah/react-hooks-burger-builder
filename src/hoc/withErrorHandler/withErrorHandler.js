68cee8244e44d25c668fc733e28ea39fb163685b
Yidah <yidahit@hotmail.com>
2021-04-02 12:09:49 +0100

    Converted to hooks withErrorHandler

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,52 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Auxi from '../Auxi/Auxi';
 
 const withErrorHandler = (WrappedComponent, axios) => {
-  return class extends Component {
-    state = {
-      error: null,
-    };
+  // anonymous functional component
+  return (props) => {
+    const [error, setError] = useState(null);
 
-    // componentDidMount works for posts requests but not get as this function executes once the components are rendered
-    // we will use use componentWillMount but as this will be removed in the future this actions can be done in the class
-    // constructor
-    componentWillMount() {
-      this.reqInterceptor=axios.interceptors.request.use((request) => {
-        // clean up error for future requests
-        this.setState({ error: null });
-        return request;
-      });
-      this.resInterceptor=axios.interceptors.response.use(response => response, (error) => {
-        this.setState({ error: error });
-      });
-    }
+    // this code executes before the content is rendered
 
-    // as we will be using withErrorHandler component 
-    // in different components, this will create 
-    // memory leaks so we need to eject interceptors 
+    const reqInterceptor = axios.interceptors.request.use((request) => {
+      // clean up error for future requests
+      setError(null);
+      return request;
+    });
+    const resInterceptor = axios.interceptors.response.use(
+      (response) => response,
+      (err) => {
+        setError(err);
+      }
+    );
+
+    // this code is run when a component is being removed from the DOM
+    // when using the clean up function of useEffect
+    // as we will be using withErrorHandler component
+    // in different components, this will create
+    // memory leaks so we need to eject interceptors
     // once are not needed
-    componentWillUnmount(){//This method is called when a component is being removed from the DOM
-      // console.log('Will Unmount', this.reqInterceptor, this.resInterceptor); //console for TEST ONLY
-      axios.interceptors.request.eject(this.reqInterceptor);
-      axios.interceptors.response.eject(this.resInterceptor);
-    }
+    useEffect(() => {
+      return () => {
+        axios.interceptors.request.eject(reqInterceptor);
+        axios.interceptors.response.eject(resInterceptor);
+      };
+    }, [reqInterceptor,resInterceptor]);
 
-    errorConfirmedHandler = () => {
-      this.setState({ error: null });
+    const errorConfirmedHandler = () => {
+      setError(null);
     };
 
-    render() {
-      return (
-        <Auxi>
-          <Modal show={this.state.error} 
-                 modalClosed={this.errorConfirmedHandler}>
-            {this.state.error ? this.state.error.message:null}
-          </Modal>
-          <WrappedComponent {...this.props} />
-        </Auxi>
-      );
-    }
+    return (
+      <Auxi>
+        <Modal show={error} modalClosed={errorConfirmedHandler}>
+          {error ? error.message : null}
+        </Modal>
+        <WrappedComponent {...props} />
+      </Auxi>
+    );
   };
 };
 
