b2f3ef1b81de11bb7cdc0bbff95fdd4bf13adece
Yidah <yidahit@hotmail.com>
2021-04-06 13:26:14 +0100

    Converted class components to hooks BurgerIngredient Modal

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './BurgerIngredient.css';
 import PropTypes from 'prop-types';
 
-class BurgerIngredient extends Component {
-  render() {
+const burgerIngredient =(props)=> {
+  
     let ingredient = null;
-    switch (this.props.type) {
+    switch (props.type) {
       case 'bread-bottom':
         ingredient = <div className={classes.BreadBottom}></div>;
         break;
@@ -34,12 +34,12 @@ class BurgerIngredient extends Component {
     }
 
     return ingredient;
-  }
+
 }
 
 // Using class component here as the favorite way to work with property validation (it can also be accomplished with functions)
-BurgerIngredient.propTypes = {
+burgerIngredient.propTypes = {
   type: PropTypes.string.isRequired,
 };
 
-export default BurgerIngredient;
+export default burgerIngredient;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Modal.css';
 import Auxi from '../../../hoc/Auxi/Auxi';
 import Backdrop from '../Backdrop/Backdrop';
 // using class components here to use "shouldcomponentupdate" update life cycle 
 // to make sure that Model does not updates unnecessarily nor the <OrderSummary/> wrapped component 
 
-class Modal extends Component{
+const modal =(props)=>{
+
   // now <OrderSummary/> updates only when the modal is displayed
   // nextProps.children - to check if spinner should be display
-  shouldComponentUpdate(nextProps, nextState){
-    return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-  }
-  // to confirm that this works we use comp
-  componentWillUpdate(){
-    // console.log('[Modal] WillUpdate')
-  }
-  render(){
+  // shouldComponentUpdate(nextProps, nextState){
+  //   return nextProps.show !== props.show || nextProps.children !== props.children;
+  // }
+
     return(  <Auxi>
-      <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+      <Backdrop show={props.show} clicked={props.modalClosed} />
       <div
         className={classes.Modal}
         style={{
-          transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: this.props.show ? '1' : '0',
+          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0',
         }}
       >
-        {this.props.children}
+        {props.children}
       </div>
     </Auxi>)
-  }
+
 }
 
-
-export default Modal;
+// With hooks if you want to check if only certain properties change not all we use memo 
+// with its own comparison function like this
+export default React.memo(modal,(prevProps, nextProps)=>nextProps.show === prevProps.show && nextProps.children === prevProps.children);
